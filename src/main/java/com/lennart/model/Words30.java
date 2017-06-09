aed59397e58f38f16953d4ea4a18d070716ed3de
LennartMac <lennartpopma@hotmail.com>
2017-06-09 08:20:14 +0200

    Added difference analysis tool to Words30

--- src/main/java/com/lennart/model/Words30.java
+++ src/main/java/com/lennart/model/Words30.java
@@ -17,8 +17,22 @@ public class Words30 {
     private double numberOfSites = 59.0;
 
     public static void main(String[] args) throws Exception {
+
+
+
+
         Words30 words30 = new Words30();
-        words30.updateDatabase();
+
+        words30.initializeDbConnection();
+        words30.combine(words30.getTop50HighestIncreaseWordCount(), words30.getTop50HighestIncreaseSiteCount());
+        words30.closeDbConnection();
+
+
+        //words30.updateDatabase();
+
+
+
+
         //words30.copyValueOfColumnsToLeft();
     }
 
@@ -193,5 +207,108 @@ public class Words30 {
             joinedMaps.put(entry.getKey(), listOccurrencesAndSites);
         }
         return joinedMaps;
+    }
+
+    private Map<String, Double> getTop50HighestIncreaseWordCount() throws Exception {
+        Map<String, Double> wordIncreaseMap = new HashMap<>();
+
+        ResultSet rs = getResultSetFromQuery("SELECT * FROM 30_words_english;");
+
+        Map<String, Double> map1 = new HashMap<>();
+        Map<String, Double> map2 = new HashMap<>();
+
+        while(rs.next()) {
+            map1.put(rs.getString("word"), rs.getDouble("29_av_no_occurr_site"));
+            map2.put(rs.getString("word"), rs.getDouble("30_av_no_occurr_site"));
+        }
+
+        for (Map.Entry<String, Double> entry : map1.entrySet()) {
+            String word = entry.getKey();
+
+            if(map2.get(word) != null) {
+                double oldWordPerSite = entry.getValue();
+                double newWordPerSite = map2.get(word);
+
+                if(oldWordPerSite >= 0.2 || newWordPerSite >= 0.2) {
+                    wordIncreaseMap.put(word, newWordPerSite / oldWordPerSite);
+                }
+            }
+        }
+
+        Map<String, Double> filteredMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> entry : wordIncreaseMap.entrySet()) {
+            if(entry.getValue() >= 2) {
+                filteredMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return HeadlineAnalyst.sortByValue(filteredMap);
+    }
+
+    private Map<String, Double> getTop50HighestIncreaseSiteCount() throws SQLException {
+        Map<String, Double> wordIncreaseMap = new HashMap<>();
+
+        ResultSet rs = getResultSetFromQuery("SELECT * FROM 30_words_english;");
+
+        Map<String, Double> map1 = new HashMap<>();
+        Map<String, Double> map2 = new HashMap<>();
+
+        while(rs.next()) {
+            map1.put(rs.getString("word"), rs.getDouble("29_av_no_sites"));
+            map2.put(rs.getString("word"), rs.getDouble("30_av_no_sites"));
+        }
+
+        for (Map.Entry<String, Double> entry : map1.entrySet()) {
+            String word = entry.getKey();
+
+            if(map2.get(word) != null) {
+                double oldWordPerSite = entry.getValue();
+                double newWordPerSite = map2.get(word);
+
+                if(oldWordPerSite >= 0.04 || newWordPerSite >= 0.04) {
+                    wordIncreaseMap.put(word, newWordPerSite / oldWordPerSite);
+                }
+            }
+        }
+
+        Map<String, Double> filteredMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> entry : wordIncreaseMap.entrySet()) {
+            if(entry.getValue() >= 2 ) {
+                filteredMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return HeadlineAnalyst.sortByValue(filteredMap);
+        //System.out.println("wacht");
+
+    }
+
+    private void combine(Map<String, Double> wordMap, Map<String, Double> siteMap) {
+        Map<String, Double> combined = new HashMap<>();
+
+        for (Map.Entry<String, Double> entry : wordMap.entrySet()) {
+            if(siteMap.get(entry.getKey()) != null) {
+                combined.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        for (Map.Entry<String, Double> entry : siteMap.entrySet()) {
+            if(combined.get(entry.getKey()) == null) {
+                if(wordMap.get(entry.getKey()) != null) {
+                    combined.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+
+        combined = HeadlineAnalyst.sortByValue(combined);
+        System.out.println("wacht");
+
+    }
+
+    private ResultSet getResultSetFromQuery(String query) throws SQLException {
+        Statement st = con.createStatement();
+        return st.executeQuery(query);
     }
 }
