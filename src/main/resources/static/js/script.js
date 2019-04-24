881cc12eefec4573b2348c9f345cc3b6c8b57d74
lennartpopma <lennart.popma@ordina.nl>
2019-04-24 10:15:59 +0200

    Auto fill written date

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -14,5 +14,60 @@ mainApp.controller('buzzwordsController', function($scope, $http) {
         $http.post('/getFollowersForDate', $scope.dateToUse).success(function(data) {
             $scope.bnData = data;
         })
+
+        setWrittenDate();
+    }
+
+    function setWrittenDate() {
+        var splittedDate = $scope.dateToUse.split("-");
+
+        var dayToUse = splittedDate[2];
+        var monthToUse = splittedDate[1];
+        var yearToUse = splittedDate[0];
+
+        if(dayToUse.includes("0")) {
+            dayToUse = dayToUse.replace("0", "");
+        }
+
+        switch(monthToUse) {
+            case '01':
+                monthToUse = "januari";
+                break;
+            case '02':
+                monthToUse = "februari";
+                break;
+            case '03':
+                monthToUse = "maart";
+                break;
+            case '04':
+                monthToUse = "april";
+                break;
+            case '05':
+                monthToUse = "mei";
+                break;
+            case '06':
+                monthToUse = "juni";
+                break;
+            case '07':
+                monthToUse = "juli";
+                break;
+            case '08':
+                monthToUse = "augustus";
+                break;
+            case '09':
+                monthToUse = "september";
+                break;
+            case '10':
+                monthToUse = "oktober";
+                break;
+            case '11':
+                monthToUse = "november";
+                break;
+            case '12':
+                monthToUse = "december";
+                break;
+        }
+
+        $scope.writtenDate = dayToUse + " " + monthToUse + " " + yearToUse;
     }
 });
