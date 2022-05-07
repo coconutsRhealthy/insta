f84d447cf8c6c4c9fd3d7ec0cbacfc50f841b38a
Lennart Popma <lennartpopma@hotmail.com>
2022-05-07 10:25:58 +0200

    Rebalance InstaAccounts May 2022

--- src/main/java/com/lennart/model/Analysis.java
+++ src/main/java/com/lennart/model/Analysis.java
@@ -1,6 +1,10 @@
 package com.lennart.model;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
 import java.sql.*;
 import java.util.*;
 
@@ -8,8 +12,82 @@ public class Analysis {
 
     private Connection con;
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(new InstaAccounts().getAllInstaAccounts().size() - new InstaAccounts().getAllInstaAccountsNew().size());
+//    public static void main(String[] args) throws Exception {
+//        new Analysis().getDataDirectiveNonDiscountGivers();
+//    }
+
+    private void removeNonGiverAccountsFromFullUserList() throws Exception {
+        List<String> nonGivers = getDataDirectiveNonDiscountGivers();
+        Collections.sort(nonGivers);
+
+        System.out.println("NonGivers complete size: " + nonGivers.size());
+
+        //nonGivers.forEach(username -> System.out.println(username));
+
+        List<String> nonGiversToRetain = new ArrayList<>();
+
+        nonGiversToRetain.add("anoukmaasofficial");
+        nonGiversToRetain.add("beaupotman");
+        nonGiversToRetain.add("bibibreijman");
+        nonGiversToRetain.add("bobbieden");
+        nonGiversToRetain.add("daniquebossers");
+        nonGiversToRetain.add("fienvermeulen");
+        nonGiversToRetain.add("ginasingels.official");
+        nonGiversToRetain.add("girlys_blog");
+        nonGiversToRetain.add("ikbensaske");
+        nonGiversToRetain.add("jaim_x");
+        nonGiversToRetain.add("jenniefromtheblog");
+        nonGiversToRetain.add("jolielot");
+        nonGiversToRetain.add("k2im");
+        nonGiversToRetain.add("kelly_weekers");
+        nonGiversToRetain.add("liekemartens");
+        nonGiversToRetain.add("lisamberr");
+        nonGiversToRetain.add("lizasips");
+        nonGiversToRetain.add("lizekorpie");
+        nonGiversToRetain.add("lizzyperridon");
+        nonGiversToRetain.add("lizzyvdligt");
+        nonGiversToRetain.add("marijezuurveld");
+        nonGiversToRetain.add("mrskeizer");
+        nonGiversToRetain.add("ninawarink");
+        nonGiversToRetain.add("onnedi");
+        nonGiversToRetain.add("roooom");
+        nonGiversToRetain.add("roxeannehazes");
+        nonGiversToRetain.add("saarkoningsberger");
+        nonGiversToRetain.add("stephsa");
+        nonGiversToRetain.add("sterrekoning");
+        nonGiversToRetain.add("sylvana");
+        nonGiversToRetain.add("tassiejs");
+        nonGiversToRetain.add("teskedeschepper");
+        nonGiversToRetain.add("vivianhoorn");
+        nonGiversToRetain.add("xies___");
+        nonGiversToRetain.add("yolanthecabau");
+
+        System.out.println("NonGivers to retain size: " + nonGiversToRetain.size());
+
+        nonGivers.removeAll(nonGiversToRetain);
+
+        System.out.println("NonGivers to remove size: " + nonGivers.size());
+
+        List<String> allAccounts = new InstaAccounts().getAllInstaAccounts();
+
+        System.out.println("AllAcconts size: " + allAccounts.size());
+
+        allAccounts.removeAll(nonGivers);
+
+        System.out.println("AllAccounts nongivers removed size: " + allAccounts.size());
+
+        Collections.sort(allAccounts);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        allAccounts.forEach(user -> System.out.println("users.add(\"" + user + "\");"));
+
+
+        //System.out.println(new InstaAccounts().getAllInstaAccounts().size() - new InstaAccounts().getAllInstaAccountsNew().size());
     }
 
     private void overallMethod() throws Exception {
@@ -43,7 +121,7 @@ public class Analysis {
     private boolean isKortingGiver(String username) throws Exception {
         KortingIdentifierPersister kortingIdentifierPersister = new KortingIdentifierPersister();
 
-        String fullHtml = kortingIdentifierPersister.getFullHtmlForUsername("picuki", username);
+        String fullHtml = kortingIdentifierPersister.getFullHtmlForUsername(username);
         Set<String> kortingWords = kortingIdentifierPersister.identifyKortingWordsUsed(fullHtml);
 
         return !kortingWords.isEmpty();
@@ -135,6 +213,198 @@ public class Analysis {
             result.put(entry.getKey(), entry.getValue());
         }
         return result;
+    }
+
+    private void getDataDirectiveFreqMap() throws Exception {
+        File file = new File("/Users/LennartMac/Documents/Projects/insta/src/main/resources/static/data_directive_okt_jan_2022.txt");
+
+        List<String> lines = readTheFile(file);
+
+        List<String> kortingGivers = new ArrayList<>();
+
+        for(String line : lines) {
+            List<String> lineList = Arrays.asList(line.split(","));
+
+            if(lineList.size() > 2) {
+                kortingGivers.add(lineList.get(2));
+            }
+        }
+
+        Set<String> kortingGiversAsSet = new HashSet<>();
+        kortingGiversAsSet.addAll(kortingGivers);
+
+        Map<String, Integer> usernameAndFreq = new HashMap<>();
+
+        for(String username : kortingGiversAsSet) {
+            usernameAndFreq.put(username, Collections.frequency(kortingGivers, username));
+        }
+
+        usernameAndFreq = sortByValueHighToLow(usernameAndFreq);
+
+        System.out.println("wacht");
+    }
+
+    private List<String> getDataDirectiveNonDiscountGivers() throws Exception {
+        File file = new File("/Users/LennartMac/Documents/Projects/insta/src/main/resources/static/data_directive_feb_may_2022.txt");
+
+        List<String> lines = readTheFile(file);
+
+        List<String> kortingGivers = new ArrayList<>();
+
+        for(String line : lines) {
+            List<String> lineList = Arrays.asList(line.split(","));
+
+            if(lineList.size() > 2) {
+                String userName = lineList.get(2);
+                userName = userName.replaceFirst(" ", "");
+                kortingGivers.add(userName);
+            }
+        }
+
+        Set<String> kortingGiversAsSet = new HashSet<>();
+        kortingGiversAsSet.addAll(kortingGivers);
+
+        List<String> allAccounts = new InstaAccounts().getAllInstaAccounts();
+        allAccounts.removeAll(kortingGiversAsSet);
+        Collections.sort(allAccounts);
+
+        allAccounts.stream().forEach(account -> System.out.println(account));
+
+        return allAccounts;
+    }
+
+    private void getCompanyFrequencies() throws Exception {
+        File file = new File("/Users/LennartMac/Documents/Projects/insta/src/main/resources/static/data_directive_nov_feb_2022.txt");
+
+        List<String> lines = readTheFile(file);
+        Map<String, Double> companiesFrequency = new HashMap<>();
+
+        for(String line : lines) {
+            if(!line.isEmpty()) {
+                String company = line.substring(2, line.indexOf(","));
+
+                if(companiesFrequency.get(company) == null) {
+                    companiesFrequency.put(company, 1.0);
+                } else {
+                    double currentValue = companiesFrequency.get(company);
+                    double newValue = currentValue + 1;
+                    companiesFrequency.put(company, newValue);
+                }
+            }
+        }
+
+        companiesFrequency = sortByValueHighToLow(companiesFrequency);
+
+        companiesFrequency.entrySet().stream().forEach(entry -> System.out.println("" + entry.getKey() + "  " + entry.getValue()));
+
+        //System.out.println("wacht");
+    }
+
+    private void analysiseCodeAndUsernameLength() throws Exception {
+        File file = new File("/Users/LennartMac/Documents/toinvestigate/discount_codes_size_analysis.txt");
+
+        List<String> lines = readTheFile(file);
+        Map<String, Integer> companies = new HashMap<>();
+        Map<String, Integer> codes = new HashMap<>();
+
+        List<String> linesToPrint = new ArrayList<>();
+
+        for(String line : lines) {
+            if(!line.isEmpty()) {
+                String company = line.substring(2, line.indexOf(","));
+                companies.put(company, company.length());
+
+                String code = line.substring(line.indexOf(",") + 2);
+                code = code.substring(0, code.indexOf(","));
+                codes.put(code, code.length());
+
+                //if(company.length() < 22 && code.length() < 22) {
+                if(company.length() < 21 && code.length() < 25) {
+                    linesToPrint.add(line);
+                } else {
+                    //System.out.println(line);
+                }
+            }
+        }
+
+        companies = sortByValueHighToLow(companies);
+        codes = sortByValueHighToLow(codes);
+
+        //linesToPrint.forEach(line -> System.out.println(line));
+
+        System.out.println("wacht");
+    }
+
+    private void analyseNewUsernamesInDataDirective() throws Exception {
+        File file = new File("/Users/LennartMac/Documents/Projects/insta/src/main/resources/static/data_directive_feb_may_2022.txt");
+
+        List<String> lines = readTheFile(file);
+        List<String> userNamesInDataDirective = new ArrayList<>();
+
+        for(String line : lines) {
+            if(!line.isEmpty()) {
+                String username = line.substring(line.indexOf(",") + 1);
+                username = username.substring(username.indexOf(",") + 1);
+                username = username.substring(1, username.indexOf(","));
+                userNamesInDataDirective.add(username);
+            }
+        }
+
+        InstaAccounts instaAccounts = new InstaAccounts();
+        List<String> allExistingAccounts = instaAccounts.getAllInstaAccounts();
+
+        userNamesInDataDirective.removeAll(allExistingAccounts);
+
+        Set<String> newUserNamesSet = new HashSet<>();
+        newUserNamesSet.addAll(userNamesInDataDirective);
+
+        List<String> sortableNewUsernames = new ArrayList<>();
+        sortableNewUsernames.addAll(newUserNamesSet);
+        Collections.sort(sortableNewUsernames);
+
+        sortableNewUsernames.forEach(username -> System.out.println(username));
+    }
+
+    private void addNewUsersToInstaList() {
+        List<String> newUsers = Arrays.asList(
+            "xxx",
+            "yyy"
+        );
+
+        InstaAccounts instaAccounts = new InstaAccounts();
+        List<String> allExistingAccounts = instaAccounts.getAllInstaAccounts();
+
+        allExistingAccounts.addAll(newUsers);
+
+        Set<String> asSet = new HashSet<>();
+        asSet.addAll(allExistingAccounts);
+
+        List<String> newUserList = new ArrayList<>();
+        newUserList.addAll(asSet);
+        Collections.sort(newUserList);
+
+        newUserList.forEach(user -> System.out.println("users.add(\"" + user + "\");"));
+    }
+
+    private List<String> readTheFile(File file) throws Exception {
+        List<String> textLines;
+        try (Reader fileReader = new FileReader(file)) {
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            String line = bufReader.readLine();
+
+            textLines = new ArrayList<>();
+
+            while (line != null) {
+                textLines.add(line);
+                line = bufReader.readLine();
+            }
+
+            bufReader.close();
+            fileReader.close();
+        }
+
+        return textLines;
     }
 
     private void initializeDbConnection() throws Exception {
