37eb9152acb3171b20ea51c9a0b8404267fab413
lennartmac <lennartpopma@hotmail.com>
2024-04-10 11:49:35 +0200

    CompanyFinder and InstaAccountFinder logic further development, including sectors etc

--- src/main/java/com/lennart/model/CompanyFinder.java
+++ src/main/java/com/lennart/model/CompanyFinder.java
@@ -1,10 +1,8 @@
 package com.lennart.model;
 
 import java.sql.*;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.sql.Date;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CompanyFinder {
@@ -13,7 +11,31 @@ public class CompanyFinder {
 
     public static void main(String[] args) throws Exception {
         //new CompanyFinder().getCodesForCompany("aybl", "2019-01-01");
-        new CompanyFinder().getCompanyFrequencyMap("2024-01-01");
+        //Map<String, Integer> companyFrequencyMap = new CompanyFinder().getCompanyFrequencyMap("2024-01-01");
+        //companyFrequencyMap.entrySet().forEach(entry -> System.out.println(entry.getKey() + " " + entry.getValue()));
+
+        //CompanyFinder companyFinder = new CompanyFinder();
+        //companyFinder.getAllDatesInDb("2024-01-01");
+        //List<String> companiesForDate = new CompanyFinder().getCompaniesForDate("2024-04-05");
+        //companyFinder.printBranchDistribution(companiesForDate);
+
+        new CompanyFinder().testMethod();
+    }
+
+    private void testMethod() throws Exception {
+        List<String> allDatesInDb = getAllDatesInDb("2024-01-01");
+
+        List<List<String>> companiesForDates = new ArrayList<>();
+
+        for(String date : allDatesInDb) {
+            List<String> companiesForDate = getCompaniesForDate(date);
+
+            if(companiesForDate.size() > 10) {
+                companiesForDates.add(getCompaniesForDate(date));
+            }
+        }
+
+        printBranchDistribution(companiesForDates);
     }
 
     private Map<String, Integer> getCompanyFrequencyMap(String dateLimit) throws Exception {
@@ -74,6 +96,193 @@ public class CompanyFinder {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
         return codesForCompany;
+    }
+
+    private void printBranchDistribution(List<List<String>> companiesForDates) {
+        Map<String, Double> branchCounts = new HashMap<>();
+
+        branchCounts.put("accessoires", 0.0);
+        branchCounts.put("cosmetics", 0.0);
+        branchCounts.put("erotics", 0.0);
+        branchCounts.put("fashion", 0.0);
+        branchCounts.put("food", 0.0);
+        branchCounts.put("homedecoration", 0.0);
+        branchCounts.put("jewellery", 0.0);
+        branchCounts.put("lingerie", 0.0);
+        branchCounts.put("other", 0.0);
+        branchCounts.put("sport", 0.0);
+        branchCounts.put("unknown", 0.0);
+
+        for(List<String> companiesForDate : companiesForDates) {
+            for(String company : companiesForDate) {
+                String branch = getBranchForCompany(company);
+                Double count = branchCounts.get(branch);
+                count++;
+                branchCounts.put(branch, count);
+            }
+        }
+
+        double totalCount = branchCounts.values().stream().mapToDouble(Double::doubleValue).sum();
+
+        List<Map.Entry<String, Double>> sortedEntries = new ArrayList<>(branchCounts.entrySet());
+        sortedEntries.sort(Map.Entry.<String, Double>comparingByValue().reversed());
+
+        sortedEntries.forEach(entry -> {
+            double percentage = (entry.getValue() / totalCount) * 100;
+            System.out.printf("%s: %.1f%%\n", entry.getKey(), percentage);
+        });
+    }
+
+    private List<String> getAllDatesInDb(String dateLimit) throws Exception {
+        Set<String> allDates = new HashSet<>();
+
+        initializeDbConnection();
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM discounts WHERE date >= '" + dateLimit + "';");
+
+        while(rs.next()) {
+            String date = rs.getString("date");
+            allDates.add(date);
+        }
+
+        rs.close();
+        st.close();
+
+        closeDbConnection();
+
+        return new ArrayList<>(allDates);
+    }
+
+    private List<String> getCompaniesForDate(String dateString) throws Exception {
+        List<String> companiesForDate = new ArrayList<>();
+
+        initializeDbConnection();
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM discounts WHERE date = '" + dateString + "';");
+
+        while(rs.next()) {
+            companiesForDate.add(rs.getString("company"));
+        }
+
+        Collections.sort(companiesForDate);
+        return companiesForDate;
+    }
+
+    private static Map<String, String> initializeCompanyToBranchMap() {
+        Map<String, String> companyToBranchMap = new HashMap<>();
+
+        companyToBranchMap.put("shein", "fashion");
+        companyToBranchMap.put("ginatricot", "fashion");
+        companyToBranchMap.put("gutsgusto", "fashion");
+        companyToBranchMap.put("burga", "accessoires");
+        companyToBranchMap.put("loavies", "fashion");
+        companyToBranchMap.put("stronger", "sport");
+        companyToBranchMap.put("gymshark", "sport");
+        companyToBranchMap.put("aybl", "sport");
+        companyToBranchMap.put("esn", "sport");
+        companyToBranchMap.put("nakdfashion", "fashion");
+        companyToBranchMap.put("bjornborg", "sport");
+        companyToBranchMap.put("myproteinnl", "sport");
+        companyToBranchMap.put("hellofresh.nl", "food");
+        companyToBranchMap.put("geurwolkje", "homedecoration");
+        companyToBranchMap.put("desenio", "homedecoration");
+        companyToBranchMap.put("snuggs", "other");
+        companyToBranchMap.put("idealofsweden", "accessoires");
+        companyToBranchMap.put("otrium", "fashion");
+        companyToBranchMap.put("zonnebrillen.com", "accessoires");
+        companyToBranchMap.put("maniacnails", "cosmetics");
+        companyToBranchMap.put("emmasleepnl", "homedecoration");
+        companyToBranchMap.put("hunkemoller", "lingerie");
+        companyToBranchMap.put("asos", "fashion");
+        companyToBranchMap.put("sellpy", "fashion");
+        companyToBranchMap.put("leolive", "other");
+        companyToBranchMap.put("icaniwill", "sport");
+        companyToBranchMap.put("only", "fashion");
+        companyToBranchMap.put("prozis", "sport");
+        companyToBranchMap.put("stevemaddeneu", "fashion");
+        companyToBranchMap.put("zalando", "fashion");
+        companyToBranchMap.put("terstal", "fashion");
+        companyToBranchMap.put("bodyandfit.com", "sport");
+        companyToBranchMap.put("lookfantastic", "cosmetics");
+        companyToBranchMap.put("lyko", "cosmetics");
+        companyToBranchMap.put("famousstore", "fashion");
+        companyToBranchMap.put("pinkgellac", "cosmetics");
+        companyToBranchMap.put("airup", "other");
+        companyToBranchMap.put("madlady", "fashion");
+        companyToBranchMap.put("arket", "fashion");
+        companyToBranchMap.put("meetmethere", "fashion");
+        companyToBranchMap.put("esuals.nl", "fashion");
+        companyToBranchMap.put("womensbest", "sport");
+        companyToBranchMap.put("photowall_sweden", "homedecoration");
+        companyToBranchMap.put("superdry", "fashion");
+        companyToBranchMap.put("easytoys", "erotics");
+        companyToBranchMap.put("loopearplugs", "other");
+        companyToBranchMap.put("paulie__pocket", "jewellery");
+        companyToBranchMap.put("edgardcooper", "other");
+        companyToBranchMap.put("greenchef.nl", "food");
+        companyToBranchMap.put("loungeunderwear", "lingerie");
+        companyToBranchMap.put("drsmile.nl", "other");
+        companyToBranchMap.put("factormeals_nl", "food");
+        companyToBranchMap.put("paulvalentine", "jewellery");
+        companyToBranchMap.put("isabelbernard", "jewellery");
+        companyToBranchMap.put("bellabarnett", "fashion");
+        companyToBranchMap.put("gustileder", "other");
+        companyToBranchMap.put("begoldennl", "jewellery");
+        companyToBranchMap.put("fittwear_official", "sport");
+        companyToBranchMap.put("wildrefill", "other");
+        companyToBranchMap.put("aliexpress", "other");
+        companyToBranchMap.put("pranamat_nl", "other");
+        companyToBranchMap.put("nike", "sport");
+        companyToBranchMap.put("bangerhead", "cosmetics");
+        companyToBranchMap.put("cocunat", "cosmetics");
+        companyToBranchMap.put("lakkiegellak", "cosmetics");
+        companyToBranchMap.put("haarspullen.nl", "cosmetics");
+        companyToBranchMap.put("icon.amsterdam", "fashion");
+        companyToBranchMap.put("nelly", "fashion");
+        companyToBranchMap.put("vitakruid", "other");
+        companyToBranchMap.put("fitshe.com", "sport");
+        companyToBranchMap.put("albelli", "homedecoration");
+        companyToBranchMap.put("safira", "jewellery");
+        companyToBranchMap.put("optimalprint", "homedecoration");
+        companyToBranchMap.put("foodforskin.care", "cosmetics");
+        companyToBranchMap.put("myfavourites.nl", "fashion");
+        companyToBranchMap.put("flink", "food");
+        companyToBranchMap.put("jotex", "homedecoration");
+        companyToBranchMap.put("livialune.nl", "fashion");
+        companyToBranchMap.put("quotrell", "fashion");
+        companyToBranchMap.put("squla", "other");
+        companyToBranchMap.put("onceuponapp", "homedecoration");
+        companyToBranchMap.put("getdrezzed", "fashion");
+        companyToBranchMap.put("doonails", "cosmetics");
+        companyToBranchMap.put("prettycurlygirl.com", "cosmetics");
+        companyToBranchMap.put("stylevana", "cosmetics");
+        companyToBranchMap.put("shampoobars.nl", "cosmetics");
+        companyToBranchMap.put("reginajewelry", "jewellery");
+        companyToBranchMap.put("veromoda", "fashion");
+        companyToBranchMap.put("luamaya", "jewellery");
+        companyToBranchMap.put("aestheticwolf", "sport");
+        companyToBranchMap.put("secretsales.nl", "fashion");
+        companyToBranchMap.put("junglueck_nl", "cosmetics");
+
+        return companyToBranchMap;
+    }
+
+    private String getBranchForCompany(String company) {
+        Map<String, String> companyToBranchMap = initializeCompanyToBranchMap();
+        String branch = companyToBranchMap.getOrDefault(company, "unknown");
+        return branch;
+    }
+
+    public static List<String> getCompaniesForBranch(String branch) {
+        Map<String, String> companyToBranchMap = initializeCompanyToBranchMap();
+
+        return companyToBranchMap.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(branch))
+                .map(Map.Entry::getKey)
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     private void initializeDbConnection() throws Exception {
--- src/main/java/com/lennart/model/InstaAccountFinder.java
+++ src/main/java/com/lennart/model/InstaAccountFinder.java
@@ -9,11 +9,109 @@ public class InstaAccountFinder {
     private Connection con;
 
     public static void main(String[] args) throws Exception {
-        List<String> influencersForApify = new InstaAccountFinder().getInfluencersForApify();
+        //List<String> influencersForApify = new InstaAccountFinder().getInfluencersForApify();
 
-        for(String influencer : influencersForApify) {
-            System.out.println("\"https://www.instagram.com/" + influencer + "\",");
+        //for(String influencer : influencersForApify) {
+        //    System.out.println("\"https://www.instagram.com/" + influencer + "\",");
+        //}
+
+        new InstaAccountFinder().fillInfluListTest();
+    }
+
+    private void fillInfluListTest() throws Exception {
+        List<String> influencers2024 = getInfluencers("2024-01-01", "2024-12-31");
+        List<String> influencersLast3Days = getInfluencers("2024-04-06", "2024-12-31");
+
+        List<String> eligibleInfluencers = influencers2024.stream()
+                .filter(i -> !influencersLast3Days.contains(i))
+                .collect(Collectors.toList());
+
+        eligibleInfluencers = removeNotToBeUsedInfluencers(eligibleInfluencers);
+
+        List<String> sectors = Arrays.asList("accessoires", "cosmetics", "erotics", "fashion", "food", "homedecoration",
+                "jewellery", "lingerie", "other", "sport");
+
+        Map<String, List<String>> influencersPerSector = sectors.stream()
+                .collect(Collectors.toMap(sector -> sector, sector -> getInfluencersForSector(sector, "2024-01-01")));
+
+        Collections.shuffle(sectors);
+
+        List<String> influencersToUse = new ArrayList<>();
+
+        for(String sector : sectors) {
+            List<String> influencersForSector = influencersPerSector.get(sector);
+            Collections.shuffle(influencersForSector);
+            List<String> selectedInfluencersForSector = influencersForSector.stream()
+                    .filter(influencer -> !influencersToUse.contains(influencer))
+                    .limit(getMaxNumberOfInfluencersForSector(sector))
+                    .collect(Collectors.toList());
+            influencersToUse.addAll(selectedInfluencersForSector);
         }
+
+        List<String> influencersWithUnknownBranch = eligibleInfluencers.stream()
+                .filter(influencer -> influencersPerSector.values().stream()
+                        .flatMap(List::stream)
+                        .noneMatch(influencer::equals))
+                .collect(Collectors.toList());
+
+        Collections.shuffle(influencersWithUnknownBranch);
+
+        int targetSize = 369;
+
+        for (String influencer : influencersWithUnknownBranch) {
+            if (influencersToUse.size() >= targetSize) {
+                break;
+            }
+
+            if (!influencersToUse.contains(influencer)) {
+                influencersToUse.add(influencer);
+            }
+        }
+
+        Collections.sort(influencersToUse);
+
+        influencersToUse.stream()
+                .map(account -> "\"https://www.instagram.com/" + account + "\",\n")
+                .forEach(System.out::print);
+    }
+
+    private long getMaxNumberOfInfluencersForSector(String sector) {
+        long maxNumber = 0L;
+
+        switch (sector) {
+            case "accessoires":
+                maxNumber = 30L;
+                break;
+            case "cosmetics":
+                maxNumber = 19L;
+                break;
+            case "erotics":
+                maxNumber = 1L;
+                break;
+            case "fashion":
+                maxNumber = 125L;
+                break;
+            case "food":
+                maxNumber = 12L;
+                break;
+            case "homedecoration":
+                maxNumber = 27L;
+                break;
+            case "jewellery":
+                maxNumber = 5L;
+                break;
+            case "lingerie":
+                maxNumber = 6L;
+                break;
+            case "other":
+                maxNumber = 23L;
+                break;
+            case "sport":
+                maxNumber = 86L;
+                break;
+        }
+
+        return maxNumber;
     }
 
     private List<String> getInfluencersForApify() throws Exception {
@@ -51,6 +149,22 @@ public class InstaAccountFinder {
         return influencersToUse;
     }
 
+    private List<String> getInfluencers(String startDate, String endDate) throws Exception {
+        initializeDbConnection();
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM discounts WHERE date >= '" + startDate + "' AND date < '" + endDate + "';");
+
+        Set<String> influencersSet = new HashSet<>();
+
+        while(rs.next()) {
+            influencersSet.add(rs.getString("influencer"));
+        }
+
+        List<String> influencers = influencersSet.stream().sorted().collect(Collectors.toList());
+        return influencers;
+    }
+
     private List<String> getInfluencers(List<String> brands, String dateLimit, int minimumPresenceInLists) throws Exception {
         List<List<String>> companyInfluencersLists = new ArrayList<>();
 
@@ -82,6 +196,79 @@ public class InstaAccountFinder {
         List<String> influencers = new ArrayList<>(influencersSet);
         Collections.sort(influencers);
         return influencers;
+    }
+
+    private List<String> getInfluencersForSector(String sector, String dateLimit) {
+        try {
+            List<String> companiesWithinSector = CompanyFinder.getCompaniesForBranch(sector);
+
+            initializeDbConnection();
+
+            Statement st = con.createStatement();
+
+            String companiesCondition = companiesWithinSector.stream()
+                    .map(company -> "company = '" + company + "'")
+                    .collect(Collectors.joining(" OR ", "(", ")"));
+
+            String query = "SELECT * FROM discounts WHERE " + companiesCondition + " AND date >= '" + dateLimit + "';";
+
+            ResultSet rs = st.executeQuery(query);
+
+            Set<String> influencersForSectorAsSet = new HashSet<>();
+
+            while(rs.next()) {
+                influencersForSectorAsSet.add(rs.getString("influencer"));
+            }
+
+            rs.close();
+            st.close();
+
+            closeDbConnection();
+
+            List<String> influencersForSector = influencersForSectorAsSet.stream().sorted().collect(Collectors.toList());
+            influencersForSector = removeNotToBeUsedInfluencers(influencersForSector);
+            return influencersForSector;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    private List<String> removeNotToBeUsedInfluencers(List<String> currentInfluList) {
+        List<String> cleanedInfluList = new ArrayList<>(currentInfluList);
+
+        List<String> usernamesToBeRemoved = Arrays.asList(
+                "janedoe",
+                "wgk",
+                "kleinpepertje",
+                "rabattk",
+                "honey",
+                "asos",
+                "rabattkodersverige",
+                "chiquelle",
+                "otrium",
+                "zalando",
+                "rabatt.inspo",
+                "rentishehu",
+                "elle",
+                "dunno",
+                "loavies",
+                "gratispandan",
+                "rabattkoder.sverige",
+                "nike",
+                "meetmethere",
+                "billigare_sverige",
+                "rabattkode.norge",
+                "nakdfashion",
+                "rabattkodsidan",
+                "d.iscount",
+                "zonnebrillen.com",
+                "eije"
+        );
+
+        cleanedInfluList.removeAll(usernamesToBeRemoved);
+
+        return cleanedInfluList;
     }
 
     public List<String> getInfluencersPresentInMultipleLists(List<List<String>> companyInfluencersLists, int minimumPresenceInLists) {
